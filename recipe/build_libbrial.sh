7f9be01a916215f11f7ead9f87498a6aaebf63ce
Isuru Fernando <isuruf@gmail.com>
2018-11-30 00:05:37 -0600

    Delete .la files

--- recipe/build_libbrial.sh
+++ recipe/build_libbrial.sh
@@ -4,6 +4,9 @@ chmod +x configure
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 
+# Get rid of any `.la`
+find $PREFIX/lib -name '*.la' -delete
+
 ./configure --prefix=$PREFIX --libdir=$PREFIX/lib --with-boost=$PREFIX --with-boost-libdir=$PREFIX/lib
 
 make
