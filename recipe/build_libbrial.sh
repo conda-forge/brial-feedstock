5e2a7e19924749a77e0468dce1a44cdcb552e26e
Isuru Fernando <isuruf@gmail.com>
2018-11-30 00:16:53 -0600

    Try fixing CFLAGS

--- recipe/build_libbrial.sh
+++ recipe/build_libbrial.sh
@@ -3,6 +3,8 @@
 chmod +x configure
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export CFLAGS=$(echo "${CFLAGS}" | sed 's|-fdebug-prefix-map.*||g')
+export CXXFLAGS=$(echo "${CXXFLAGS}" | sed 's|-fdebug-prefix-map.*||g')
 
 # Get rid of any `.la`
 find $PREFIX/lib -name '*.la' -delete
