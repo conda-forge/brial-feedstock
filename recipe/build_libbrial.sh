002133d1412a33d2f06dfd1012a9f478bf646160
Isuru Fernando <isuruf@gmail.com>
2020-11-15 12:18:16 -0600

    config.sub update

--- recipe/build_libbrial.sh
+++ recipe/build_libbrial.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 chmod +x configure
 
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 sed -i.bak 's|-fdebug-prefix-map.*||g' "${PREFIX}/lib/pkgconfig/m4ri.pc"
 rm  "${PREFIX}/lib/pkgconfig/m4ri.pc.bak"
 
 # Get rid of any `.la`
 find $PREFIX/lib -name '*.la' -delete
 
-./configure --prefix=$PREFIX --libdir=$PREFIX/lib --with-boost=$PREFIX --with-boost-libdir=$PREFIX/lib
+./configure --prefix=$PREFIX --libdir=$PREFIX/lib --with-boost=$PREFIX --with-boost-libdir=$PREFIX/lib --disable-static
 
 make -j${CPU_COUNT}
 make check -j${CPU_COUNT}
